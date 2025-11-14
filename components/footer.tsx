49334e0a6b1671d9899643ccebb61311bb841f20
Xpression <emmybanks03@gmail.com>
2025-11-14 01:11:12 +0100

    added discord social

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { FaWhatsapp, FaTwitter, FaInstagram, FaTrademark } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
+import { SiGmail, SiDiscord } from "react-icons/si";
+
 import Link from "next/link";
 
 export const Footer = () => {
@@ -27,7 +28,7 @@ export const Footer = () => {
         <div className="flex gap-5 text-gray-400">
           <Link href="https://wa.me/+2349031493999" className="hover:text-white transition-colors"><FaWhatsapp size={18} /></Link>
           <Link href="https://x.com/emperor_dev01" className="hover:text-white transition-colors"><FaTwitter size={18} /></Link>
-          <Link href="https://instagram.com/only_emperor_00" className="hover:text-white transition-colors"><FaInstagram size={18} /></Link>
+          <Link href="https://discord.gg/S8xesNw5" className="hover:text-white transition-colors"><SiDiscord size={18} /></Link>
           <Link href="mailto:emmybanks03@gmail.com" className="hover:text-white transition-colors"><SiGmail size={18} /></Link>
         </div>
       </div>
